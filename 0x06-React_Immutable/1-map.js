55a92ecf370ca91fb546cb7117875f1b778cbdcb
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-11-20 23:49:52 +0100

    (feat) -> using Map create immutable object from Immutable js

--- 0x06-React_Immutable/1-map.js
+++ 0x06-React_Immutable/1-map.js
@@ -1,3 +1,3 @@
 import Immutable from './node_modules/immutable/dist/immutable.js';
 
-export const getImmutableObject = (obv) => Immutable.fromJS(obv);
+export const getImmutableObject = (obv) => Immutable.Map(obv);
